c955575ffe2cdc170ec521c5bb1169008f8ae035
Nikolay Yaremko <makiwara@humanemagica.com>
2014-04-29 11:06:29 +0600

    SVG icons with PNG fallback

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+/* Prepare icons */
+var data = [ 
+    '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+    'practice-1-books', 'practice-2-lecture', 'practice-3-research', 'practice-4-studies', 'practice-5-reflect' ];
+var fallback = {}, placement = {};
+for (var i=0; i<data.length; i++) {
+    fallback['symbol-'+data[i]] = data[i] + '.png';
+    placement['.icon-'+data[i]] = 'symbol-'+data[i];
+}
+$.svgIcons('img/icons/icons.svg', { 
+        w: 64,
+        h: 64,
+        fallback_path: 'img/icons/', 
+        fallback: fallback,
+        placement: placement
+})
